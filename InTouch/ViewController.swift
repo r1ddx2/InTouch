654ba456a877adc3c4dd659a92da5ed9c01d9e54
Red Wang <redxoxored@gmail.com>
2023-11-16 15:11:19 +0800

    Initial commit

---
+++ InTouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InTouch
+//
+//  Created by Red Wang on 2023/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
