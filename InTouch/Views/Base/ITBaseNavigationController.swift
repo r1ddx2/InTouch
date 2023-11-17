dc4bcfd195b7289e380a0b86a831154eff738c11
Red Wang <redxoxored@gmail.com>
2023-11-17 20:39:53 +0800

    feat: Create `ITBaseNavigationController`

--- InTouch/Views/Base/ITBaseNavigationController.swift
+++ InTouch/Views/Base/ITBaseNavigationController.swift
@@ -4,5 +4,17 @@
 //
 //  Created by Red Wang on 2023/11/17.
 //
+import UIKit
 
-import Foundation
+class ITBaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.backgroundColor = .white
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        navigationBar.standardAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        navigationBar.isTranslucent = false
+    }
+}
