e9da0600d439bd77293170b7d0ef2f164ae4081b
Red Wang <redxoxored@gmail.com>
2023-11-17 20:52:54 +0800

    feat: UIView extension

--- InTouch/Extensions/UIView + Extension.swift
+++ InTouch/Extensions/UIView + Extension.swift
@@ -5,4 +5,41 @@
 //  Created by Red Wang on 2023/11/17.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+extension UIView {
+    
+    // Border Color
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let borderColor = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: borderColor)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    // Border width
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    // Corner radius
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
