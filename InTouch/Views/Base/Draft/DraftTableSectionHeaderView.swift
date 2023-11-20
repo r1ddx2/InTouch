fa47103e78452ab770c4b3534208058686c000eb
Red Wang <redxoxored@gmail.com>
2023-11-20 10:31:23 +0800

    feat: Add DraftTableSectionHeaderView for section header

--- InTouch/Views/Base/Draft/DraftTableSectionHeaderView.swift
+++ InTouch/Views/Base/Draft/DraftTableSectionHeaderView.swift
@@ -5,4 +5,33 @@
 //  Created by Red Wang on 2023/11/18.
 //
 
-import Foundation
+import UIKit
+
+class DraftTableSectionHeaderView: UITableViewHeaderFooterView {
+    
+    static let identifier = "DraftTableSectionHeaderView"
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.font = .regular(size: 16)
+        label.textColor = .ITBlack
+        return label
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setupLayout()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setupLayout() {
+        contentView.backgroundColor = .white
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
