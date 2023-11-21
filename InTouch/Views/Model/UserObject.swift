528364ac5c41ac6569526ffb2eac72564c7d0fae
Red Wang <redxoxored@gmail.com>
2023-11-21 16:55:12 +0800

    feat: Add `UserObject` data model

--- InTouch/Views/Model/UserObject.swift
+++ InTouch/Views/Model/UserObject.swift
@@ -5,45 +5,73 @@
 //  Created by Red Wang on 2023/11/20.
 //
 
-import Foundation
+import UIKit
+import FirebaseFirestore
 
-struct User {
-    let archived: [ArchivedPost]
-    let groups: [String]
-    let post: Post
+struct User: Codable {
+    var userID: String
+    var userName: String
+    var userIcon: String
+    var userCover: String
+    var archived: [ArchivedPost]?
+    var groups: [String]?
+    var weeklyPost: [Post]?
+    
+    enum CodingKeys: String, CodingKey {
+        case userID = "user_id"
+        case userName = "user_name"
+        case userIcon = "user_icon"
+        case userCover = "user_cover"
+        case archived
+        case groups
+        case weeklyPost = "weekly_post"
+    }
 }
 
-struct ArchivedPost {
-    let date: Date
-    let postID: String
-    let userID: String
-    let userName: String
-    let imageBlocks: [ImageBlock]
-    let textBlocks: [TextBlock]
+struct ArchivedPost: Codable {
+    var date: Date
+    var postID: String
+    var userID: String
+    var userName: String
+    var imageBlocks: [ImageBlock]
+    var textBlocks: [TextBlock]
 }
 
-struct Post {
-    let date: Date
-    let postID: String
-    let userID: String
-    let userName: String
-    let imageBlocks: [ImageBlock]
-    let textBlocks: [TextBlock]
+struct Post: Codable {
+    var date: Timestamp
+    var postID: String
+    var userID: String
+    var userName: String
+    var imageBlocks: [ImageBlock]
+    var textBlocks: [TextBlock]
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case postID = "post_id"
+        case userID = "user_id"
+        case userName = "user_name"
+        case imageBlocks = "image_blocks"
+        case textBlocks = "text_blocks"
+       
+    }
 }
 
-struct ImageBlock {
-    let caption: String
-    let image: String
-    let location: GeoPoint
-    let place: String
+struct ImageBlock: Codable {
+    var caption: String
+    var image: String
+    var location: GeoPoint?
+    var place: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case caption
+        case image
+        case location
+        case place
+    }
 }
 
-struct TextBlock {
-    let title: String
-    let content: String
+struct TextBlock: Codable {
+    var title: String
+    var content: String
 }
 
-struct GeoPoint {
-    let latitude: Double
-    let longitude: Double
-}
