32f5cb4567106304956857bb603e595cf4b51014
Red Wang <redxoxored@gmail.com>
2023-11-24 13:22:44 +0800

    feat: Add new data model `Group`

--- InTouch/Views/Model/GroupObject.swift
+++ InTouch/Views/Model/GroupObject.swift
@@ -5,4 +5,22 @@
 //  Created by Red Wang on 2023/11/23.
 //
 
-import Foundation
+import UIKit
+import FirebaseFirestore
+
+
+struct Group: Codable {
+    var groupName: String
+    var groupId: String
+    var groupIcon: String
+    var groupCover: String
+    var members: [User]
+    
+    enum CodingKeys: String, CodingKey {
+        case groupName = "group_name"
+        case groupId = "group_id"
+        case groupIcon = "group_icon"
+        case groupCover = "group_cover"
+        case members
+    }
+}
--- InTouch/Views/Model/NewsLetterObject.swift
+++ InTouch/Views/Model/NewsLetterObject.swift
@@ -5,4 +5,64 @@
 //  Created by Red Wang on 2023/11/23.
 //
 
-import Foundation
+
+import UIKit
+import FirebaseFirestore
+
+
+struct NewsLetter: Codable {
+    var date: Date
+    var newsId: String
+    var newsCover: String
+    var posts: [Post]
+    var title: String
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case newsId = "news_id"
+        case newsCover = "news_cover"
+        case posts
+        case title
+    }
+    
+}
+
+struct Post: Codable {
+    var date: Date
+    var postId: String
+    var userId: String
+    var userIcon: String
+    var userName: String
+    var imageBlocks: [ImageBlock]
+    var textBlocks: [TextBlock]
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case postId = "post_id"
+        case userId = "user_id"
+        case userIcon = "user_icon"
+        case userName = "user_name"
+        case imageBlocks = "image_blocks"
+        case textBlocks = "text_blocks"
+    }
+}
+
+struct ImageBlock: Codable {
+    var caption: String
+    var image: String
+    var location: GeoPoint?
+    var place: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case caption
+        case image
+        case location
+        case place
+    }
+}
+
+struct TextBlock: Codable {
+    var title: String
+    var content: String
+}
+
