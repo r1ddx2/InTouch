3b3dbe3ee1b561d6e9b50db17415a96333044108
Red Wang <redxoxored@gmail.com>
2023-11-24 13:32:18 +0800

    feat: Add `UserBlockView` to show user info in posts

--- InTouch/Views/Home/UserBlockView.swift
+++ InTouch/Views/Home/UserBlockView.swift
@@ -5,4 +5,43 @@
 //  Created by Red Wang on 2023/11/24.
 //
 
-import Foundation
+import UIKit
+
+class UserBlockView: UIView {
+    // MARK: - Subviews
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(resource: .iconProfileSelected)
+        imageView.layer.cornerRadius = 30
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Panda"
+        label.font = .medium(size: 18)
+        label.textColor = .ITBlack
+        return label
+    }()
+    
+    // MARK: - View Load
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpLayouts()
+        setUpActions()
+    }
+    private func setUpLayouts() {
+        
+    }
+    private func setUpActions() {
+        
+    }
+    
+    
+    // MARK: - Methods
+    
+
+}
