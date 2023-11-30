1bc4cc34e9bf93fa935109c12153aa941bf8a0a9
Red Wang <redxoxored@gmail.com>
2023-11-30 10:39:43 +0800

    feat: Add get last week date range for HomePage

--- InTouch/Extensions/Date + Extension.swift
+++ InTouch/Extensions/Date + Extension.swift
@@ -20,6 +20,15 @@ extension Date {
         let (startDate, endDate) = calculateDateRange(for: self)
         return "\(formatDate(startDate)) - \(formatDate(endDate))"
     }
+    
+   
+    func getLastWeekDateRange() -> String {
+        let (startDate, endDate) = calculateDateRange(for: self)
+        let calendar = Calendar.current
+        let startDateOfLastWeek = calendar.date(byAdding: .weekOfYear, value: -1, to: startDate)!
+        let endDateOfLastWeek = calendar.date(byAdding: .weekOfYear, value: -1, to: endDate)!
+        return "\(formatDate(startDateOfLastWeek)) - \(formatDate(endDateOfLastWeek))"
+    }
 
     private func calculateDateRange(for date: Date) -> (Date, Date) {
         let calendar = Calendar.current
@@ -29,7 +38,9 @@ extension Date {
         let endDate = calendar.date(byAdding: .day, value: 6, to: startDate)!
         return (startDate, endDate)
     }
-
+    
+ 
+    
     private func formatDate(_ date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy.MM.dd"
