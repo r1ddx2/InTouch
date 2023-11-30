fc4e051e584dceecb649b160f0e8f22e489ab671
Red Wang <redxoxored@gmail.com>
2023-11-30 10:44:14 +0800

    feat: Add User info blocks on each post

--- InTouch/Views/Home/UserBlockView.swift
+++ InTouch/Views/Home/UserBlockView.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class UserBlockView: UIView {
     // MARK: - Subviews
-    let iconImageView: UIImageView = {
+    let userIconImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(resource: .iconProfileSelected)
-        imageView.layer.cornerRadius = 30
+        imageView.layer.cornerRadius = 20
         imageView.clipsToBounds = true
         return imageView
     }()
     let userNameLabel: UILabel = {
         let label = UILabel()
-        label.text = "Panda"
-        label.font = .medium(size: 18)
+        label.font = .medium(size: 16)
         label.textColor = .ITBlack
         return label
+    }()
+    let moreOptionsButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(resource: .iconMoreOptions), for: .normal)
+        return button
     }()
     
     // MARK: - View Load
@@ -34,13 +38,27 @@ class UserBlockView: UIView {
         setUpActions()
     }
     private func setUpLayouts() {
+        addSubview(userIconImageView)
+        addSubview(userNameLabel)
+        addSubview(moreOptionsButton)
         
+        userIconImageView.snp.makeConstraints { (make) -> Void in
+            make.centerY.equalTo(self.snp.centerY)
+            make.left.equalTo(self).offset(16)
+            make.height.width.equalTo(40)
+        }
+        userNameLabel.snp.makeConstraints { (make) -> Void in
+            make.centerY.equalTo(userIconImageView.snp.centerY)
+            make.left.equalTo(userIconImageView.snp.right).offset(16)
+        }
+        moreOptionsButton.snp.makeConstraints { (make) -> Void in
+            make.centerY.equalTo(userIconImageView.snp.centerY)
+            make.right.equalTo(self).offset(-12)
+            make.height.width.equalTo(30)
+        }
     }
     private func setUpActions() {
-        
     }
-    
-    
     // MARK: - Methods
     
 
