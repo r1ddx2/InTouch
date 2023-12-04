cac7da05e9c96bbd270325bf014a87d7230ffd18
Red Wang <redxoxored@gmail.com>
2023-12-04 16:57:31 +0800

    feat: Add CLLocationCoordinate2D extension to convert coordinates

--- InTouch/Extensions/CLLocationCoordinate2D + Extension.swift
+++ InTouch/Extensions/CLLocationCoordinate2D + Extension.swift
@@ -4,5 +4,11 @@
 //
 //  Created by Red Wang on 2023/12/2.
 //
+import CoreLocation
+import FirebaseFirestore
 
-import Foundation
+extension CLLocationCoordinate2D {
+    func toGeoPoint() -> GeoPoint {
+        return GeoPoint(latitude: self.latitude, longitude: self.longitude)
+    }
+}
