df706135f6bf4865bb4dfc7ab289b7c90b607fb2
Red Wang <redxoxored@gmail.com>
2023-12-25 13:44:07 +0800

    fix: Delete resused codes

--- InTouch/Views/Manager/CloudStorageManager.swift
+++ InTouch/Views/Manager/CloudStorageManager.swift
@@ -4,10 +4,10 @@
 //
 //  Created by Red Wang on 2023/11/21.
 //
-import UIKit
 import Firebase
 import FirebaseFirestore
 import FirebaseStorage
+import UIKit
 
 enum CSError: Error {
     case invalidType
@@ -17,35 +17,34 @@ enum CSError: Error {
 
 class CloudStorageManager {
     typealias CompletionHandler<T> = (_ result: Result<T, Error>) -> Void
-    
+
     static let shared = CloudStorageManager()
     private let storage = Storage.storage()
     private let db = Firestore.firestore()
-    
-    // MARK: - Upload Image
-    
-    func uploadImages(
+
+    // MARK: - Method
+
+    func uploadURL(
         fileUrl: URL,
-        userId: String,
+        filePathString: String,
         completion: @escaping CompletionHandler<String>
     ) {
-        
         let timestamp = Int(Date().timeIntervalSince1970 * 1000)
         let fileExtension = fileUrl.pathExtension
-        let filePath = "\(userId)/\(timestamp).\(fileExtension)"
+        let filePath = "\(filePathString)/\(timestamp).\(fileExtension)"
         let storageRef = storage.reference().child(filePath)
-        
+
         storageRef.putFile(
             from: fileUrl,
             metadata: nil,
-            completion: { (storageMetaData, error) in
-                if let error = error {
+            completion: { _, error in
+                if let _ = error {
                     completion(.failure(CSError.uploadFail))
                     return
                 }
-                
-                storageRef.downloadURL { (url, error) in
-                    if let error = error  {
+
+                storageRef.downloadURL { url, error in
+                    if let _ = error {
                         completion(.failure(CSError.downloadFail))
                         return
                     }
@@ -55,81 +54,7 @@ class CloudStorageManager {
                     }
                     completion(.failure(CSError.invalidType))
                 }
-                
-        })
-        
-    }
-         
-    func uploadGroupImages(
-        fileUrl: URL,
-        groupId: String,
-        completion: @escaping CompletionHandler<String>
-    ) {
-        
-        let timestamp = Int(Date().timeIntervalSince1970 * 1000)
-        let fileExtension = fileUrl.pathExtension
-        let filePath = "\(groupId)/\(timestamp).\(fileExtension)"
-        let storageRef = storage.reference().child(filePath)
-        
-        storageRef.putFile(
-            from: fileUrl,
-            metadata: nil,
-            completion: { (storageMetaData, error) in
-                if let error = error {
-                    completion(.failure(CSError.uploadFail))
-                    return
-                }
-                
-                storageRef.downloadURL { (url, error) in
-                    if let error = error  {
-                        completion(.failure(CSError.downloadFail))
-                        return
-                    }
-                    if let urlString = url?.absoluteString {
-                        completion(.success(urlString))
-                        return
-                    }
-                    completion(.failure(CSError.invalidType))
-                }
-                
-        })
-        
-    }
-    // MARK: - Audio
-    
-    func uploadAudio(
-        fileUrl: URL,
-        userId: String,
-        completion: @escaping CompletionHandler<String>
-    ) {
-        let timestamp = Int(Date().timeIntervalSince1970 * 1000)
-        let fileExtension = fileUrl.pathExtension
-        let filePath = "\(userId)/\(timestamp).\(fileExtension)"
-        let storageRef = storage.reference().child(filePath)
-        
-        storageRef.putFile(
-            from: fileUrl,
-            metadata: nil,
-            completion: { (storageMetaData, error) in
-                if let error = error {
-                    completion(.failure(CSError.uploadFail))
-                    return
-                }
-                
-                storageRef.downloadURL { (url, error) in
-                    if let error = error  {
-                        completion(.failure(CSError.downloadFail))
-                        return
-                    }
-                    if let urlString = url?.absoluteString {
-                        completion(.success(urlString))
-                        return
-                    }
-                    completion(.failure(CSError.invalidType))
-                }
-                
-        })
-        
+            }
+        )
     }
 }
-
